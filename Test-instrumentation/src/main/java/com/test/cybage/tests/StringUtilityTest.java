a994c036ea041fbee70d4275dc75142c36d267d2
sumanta <rout.sumanta@gmail.com>
2013-06-21 13:38:39 +0530

    Added First time

---
+++ Test-instrumentation/src/main/java/com/test/cybage/tests/StringUtilityTest.java
@@ -0,0 +1,21 @@
+package com.test.cybage.tests;
+
+import android.test.AndroidTestCase;
+import android.test.suitebuilder.annotation.SmallTest;
+
+public class StringUtilityTest extends AndroidTestCase {
+
+	public StringUtilityTest() {
+		super();
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	@SmallTest
+	public void testUtilityConvertInt1() {
+		assertTrue(true);
+	}
+}
